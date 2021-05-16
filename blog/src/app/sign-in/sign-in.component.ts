fb1613825d76f6494c0cc77f6337b32451dc9164
eechol <726154403@qq.com>
2021-05-16 17:34:57 +0800

    1

--- blog/src/app/sign-in/sign-in.component.ts
+++ blog/src/app/sign-in/sign-in.component.ts
@@ -14,7 +14,7 @@ export class SignInComponent implements OnInit {
   ngOnInit() {
   }
   onSubmit(value){
-    let string = "master and list string";
+    let string = "master string";
     let routerService = this.router;
     let user = new User();
     user.username = value.username;
